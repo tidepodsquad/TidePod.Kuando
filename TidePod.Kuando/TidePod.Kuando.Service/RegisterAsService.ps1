c626222840cf82a22e8c95fbfa65112731919bad
Max Drexel <mdrexel@osisoft.com>
2020-03-04 17:13:02 -0500

    Add script and start shared library

---
+++ TidePod.Kuando/TidePod.Kuando.Service/RegisterAsService.ps1
@@ -0,0 +1,3 @@
+﻿# Put this script & the service executable in the folder the service should run from, and then run this script to register the service.
+$location = Get-Location
+New-Service -name "TidePod Kuando Master Control" -binaryPathName "$location\TidePod.Kuando.Service.exe"
